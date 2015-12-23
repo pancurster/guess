eec2608d13651078bae2437526d523099e6de7bb
Lukasz Panek <pancurster@gmail.com>
2015-12-23 16:53:13 +0100

    Small Erlang notation improvement

--- guess.erl
+++ guess.erl
@@ -1,22 +1,18 @@
 -module(guess).
 -export([main/0]).
 
-check(Guess, Answer) when Guess == Answer ->
-    io:format("You Win!~n"),
-    true;
-check(Guess, Answer) when Guess > Answer ->
-    io:format("Smaller~n"),
-    false;
-check(Guess, Answer) when Guess < Answer ->
-    io:format("Bigger~n"),
-    false.
+check(Guess, Answer) when Guess ==Answer -> {true, "You Win!"};
+check(Guess, Answer) when Guess > Answer -> {false, "Smaller"};
+check(Guess, Answer) when Guess < Answer -> {false, "Bigger"}.
 
 guess() ->
     {_, [Guess]} = io:fread("Guess number I think of:", "~d"),
     Guess.
 
 loop(Iter, Answer, false) ->
-    loop(Iter + 1, Answer, check(guess(), Answer));
+    {Result, InfoStr} = check(guess(), Answer),
+    io:format("~s~n", [InfoStr]),
+    loop(Iter + 1, Answer, Result);
 loop(Iter, _, true) ->
     io:format("Score:~p", [Iter]),
     halt().
