d3c50dfaf41c6968b8b48c8adadd68451aa2bfa0
Lukasz Panek <pancurster@gmail.com>
2015-12-25 22:40:39 +0100

    Erlang version improvement
    * closure
    * lambda function

--- guess.erl
+++ guess.erl
@@ -1,15 +1,14 @@
 -module(guess).
 -export([main/0]).
 
-guess(Answer) ->
-    case io:fread("Guess number I think of:", "~d") of
-        {_, [Guess]} when Guess ==Answer -> {true, "You Win!"};
-        {_, [Guess]} when Guess > Answer -> {false, "Smaller"};
-        {_, [Guess]} when Guess < Answer -> {false, "Bigger"}
-    end.
-
 loop(false, Score, Answer) ->
-    {Result, InfoStr} = guess(Answer),
+    {Result, InfoStr} = (fun() ->
+        case io:fread("Guess number I think of:", "~d") of
+            {ok, [Guess]} when Guess ==Answer -> {true, "You Win!"};
+            {ok, [Guess]} when Guess > Answer -> {false, "Smaller"};
+            {ok, [Guess]} when Guess < Answer -> {false, "Bigger"}
+        end
+    end)(),
     io:format("~s~n", [InfoStr]),
     loop(Result, Score + 1, Answer);
 loop(true, Score, _) ->
